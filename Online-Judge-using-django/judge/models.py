500b1a3a99990870d9b3357ba881f7934d7d9fd3
Hanuman_36 <59814996+hanumannath@users.noreply.github.com>
2022-07-11 16:27:37 +0530

    Add files via upload

---
+++ Online-Judge-using-django/judge/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
+class problem(models.Model):
+    name=models.TextField()
+    statement = models.TextField()
+    difficulty=models.TextField()
+    def __str__(self):
+        return self.name
+
+
+class solution(models.Model):
+    curr_problem=models.ForeignKey(problem, on_delete=models.CASCADE, null=True)
+    verdict=models.TextField(null=True)
+    time_of_submit = models.DateTimeField(auto_now=True)
+    code=models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.curr_problem
+    
+    
+
+class testcase(models.Model):
+    curr_problem=models.ForeignKey(problem, on_delete=models.CASCADE,null=True)
+    input=models.TextField()
+    output=models.TextField()
