f989af7e39da74a4382f353b088e24003af53d75
Hanuman Sidh <hanumannath44420@gmail.com>
2022-08-15 15:55:32 +0530

    changed something in database

--- judge/Testcases/question1/sol.cpp
+++ judge/Testcases/question1/sol.cpp
@@ -1,13 +1,12 @@
 #include<iostream>
-using namespace std;
-int main(){
-int n,t;
+ using namespace std;
+ int main()
+{ int n,t; 
 cin>>n;
-int arr[n];
+ int arr[n]; 
 for(int i=0;i<n;i++){
-cin>>arr[i];
-}
-cin>>t;
-cout<<"True";
-return 0;
+ cin>>arr[i]; }
+ cin>>t; 
+cout<<"True"; 
+return 0; 
 }
